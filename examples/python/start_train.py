3ae80b313e9e0a830554cc99739bf92abaf82471
Rikard Strand <rixtan@gmail.com>
2017-02-26 21:35:43 +0100

    Exampel of sending IR commands

---
+++ examples/python/start_train.py
@@ -0,0 +1,16 @@
+"""
+Start the train configured with channel 3 Blue
+"""
+
+# External librarys
+from subprocess import call
+
+def send_once(command):
+    """
+    Send single call to IR LED.
+    """
+    call(['irsend', 'SEND_ONCE', 'LEGO_Single_Output', command])
+
+
+send_once('3B_5')
+
